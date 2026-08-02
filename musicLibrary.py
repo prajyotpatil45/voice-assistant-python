cc6209bedaab36b70bf86c355feedda809f53d4b
prajyot patil <pprajyot256@gmail.com>
2026-08-02 21:05:19 +0530

    add new song url tenu sang rakhna

--- musicLibrary.py
+++ musicLibrary.py
@@ -2,5 +2,6 @@ songs = {
     "saathiya": "https://youtu.be/2ibPLYwVM38?si=mXYu8S68Fx89BXN8",
     "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw",
     "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
-    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21"
+    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21",
+    "tenu sang rakhna":"https://youtu.be/OkpIoEC44kk?si=GirJeAirWJF8ibxl"
 }
